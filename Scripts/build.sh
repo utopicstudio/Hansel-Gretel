668bc0be50069bc41c6de3d8756cbf0538e28c6f
Pandalex1 <34633112+Pandalex1@users.noreply.github.com>
2020-03-02 17:30:07 -0300

    New travis, build, ainstall, postbuild and upload

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,36 +1,56 @@
 #! /bin/sh
 
-project="UtopicBuild"
+# NOTE the command args below make the assumption that your Unity project folder is
+#  a subdirectory of the repo root directory, e.g. for this repo "unity-ci-test" 
+#  the project folder is "UnityProject". If this is not true then adjust the 
+#  -projectPath argument to point to the right location.
 
-echo "Attempting to build $project for Windows"
+## Run the editor unit tests
+echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-runEditorTests \
+	-editorTestsResultFile $(pwd)/test.xml \
+	-quit
 
-echo "Attempting to build $project for OS X"
+rc0=$?
+echo "Unit test logs"
+cat $(pwd)/test.xml
+# exit if tests failed
+if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+
+## Make the builds
+# Recall from install.sh that a separate module was needed for Windows build support
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
+	-quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
-
-echo 'Logs from build'
+rc1=$?
+echo "Build logs (Windows)"
 cat $(pwd)/unity.log
+
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/unity.log \
+	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+	-quit
+
+rc2=$?
+echo "Build logs (OSX)"
+cat $(pwd)/unity.log
+
+exit $(($rc1|$rc2))
