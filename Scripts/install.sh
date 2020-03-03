144255c7634cfe163cffbae0490feb5494ba64c4
Pandalex1 <34633112+Pandalex1@users.noreply.github.com>
2020-03-03 16:36:31 -0300

    Changed comments on install version

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Download Unity3D installer into the container
-#  The below link will need to change depending on the version, this one is for 5.5.1
+#  The below link will need to change depending on the version, this one is for 2018.3.3f1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
 
 echo 'Downloading Unity 2018.3.3f1 pkg:'
