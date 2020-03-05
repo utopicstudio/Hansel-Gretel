6a26f452f20b7c6a23179a87354bd34b14a87185
Pandalex1 <34633112+Pandalex1@users.noreply.github.com>
2020-03-04 22:53:26 -0300

    Changed download from 2018.3.3f1 to 2019.2.0f1

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,8 +4,8 @@
 #  The below link will need to change depending on the version, this one is for 2018.3.3f1
 #  Refer to https://unity3d.com/get-unity/download/archive and find the link pointed to by Mac "Unity Editor"
 
-echo 'Downloading Unity 2018.3.3f1 pkg:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/393bae82dbb8/MacEditorInstaller/Unity-2018.3.3f1.pkg
+echo 'Downloading Unity 2019.2.0f1 pkg:'
+curl --retry 5 -o Unity.pkg https://unity3d.com/get-unity/download?thank-you=update&download_nid=62773&os=Mac
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
